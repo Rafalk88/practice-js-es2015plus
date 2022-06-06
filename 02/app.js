37a0cc98d1121f58c2350a8569a3716f5a678f34
Rafał Kochanecki <rafal.kochanecki@gmail.com>
2022-06-06 19:53:43 +0200

    task 2 done

--- 02/app.js
+++ 02/app.js
@@ -1,3 +1,33 @@
-const inputEvent = require('./inputListener.js')
+document.addEventListener('DOMContentLoaded', () => {
+console.log('DOM')
 
-inputEvent()
+const btnEl = document.querySelector('.cart__btn-calculate')
+const totalPrice = document.querySelector('.cart__total-price')
+const inputList = document.getElementsByClassName('cart__quantity')
+
+if (btnEl && totalPrice) {
+    
+    btnEl.addEventListener(
+
+        'click',
+        () => {
+
+        let total = 0;
+        [...inputList].forEach( 
+        
+                item => {
+            
+                    const {dataset: {price}, value} = item
+                    total += price * value
+                    totalPrice.innerText = total
+                
+                }
+
+        )
+
+        }
+
+    )
+}
+
+})
