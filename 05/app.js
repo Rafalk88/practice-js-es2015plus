66c5265531c03a202163778d27d1b4f8a6aa5b22
Rafał Kochanecki <rafal.kochanecki@gmail.com>
2022-06-07 21:14:57 +0200

    Task 5 done

--- 05/app.js
+++ 05/app.js
@@ -1,4 +1,7 @@
-import Programmer from './classes/Programmer';
+import Programmer from './classes/Programmer.js';
+import Junior from './classes/Junior.js';
+import Mid from './classes/Mid.js';
+import Senior from './classes/Senior.js';
 
 const features = {
     skills: 10, 
@@ -11,6 +14,21 @@ const task = {
     size: 1,
 }
 
+const features2 = {}
+const task2 = {difficult: 2, size: 1,}
+
 const programmer = new Programmer( features );
 const time = programmer.getApproximateTimeTaskDoneInHours( task );
 console.log(time);
+
+const junior = new Junior(features2)
+const mid = new Mid(features2)
+const senior = new Senior(features2)
+
+const time2 = junior.getApproximateTimeTaskDoneInHours( task2 )
+const time3 = mid.getApproximateTimeTaskDoneInHours( task2 )
+const time4 = senior.getApproximateTimeTaskDoneInHours( task2 )
+
+console.log(time2)
+console.log(time3)
+console.log(time4)
